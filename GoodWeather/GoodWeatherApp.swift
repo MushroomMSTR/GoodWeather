7ef8092a64b32f5f437d7ac54364daa1c449420f
NazarStf <hworker98@gmail.com>
2023-06-03 12:42:31 +0300

    Initial Commit

---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by NazarStf on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
